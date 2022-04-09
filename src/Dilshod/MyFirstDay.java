8fa53a5bcdb8a0acd919dd183b69edfafb450a1d
dilshoddostiev <dilshoddostiev@hotmail.com>
2022-04-09 16:55:19 -0500

    Updated 5

---
+++ src/Dilshod/MyFirstDay.java
@@ -0,0 +1,16 @@
+package Dilshod;
+
+public class MyFirstDay {
+
+    public static void main(String[] args) {
+        // Updating My Smoke Testing
+        // Khosiyat , make sure you run your tests everyday
+        // Khosiyat please make sure iamm on the List
+
+        // updates done
+
+        //update again
+    }
+}
+
+
